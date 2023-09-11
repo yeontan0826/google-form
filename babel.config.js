f316ad2b60bbc62cdca3e9df427efd43ededa604
yeontan0826 <yeontan0826@gmail.com>
2023-09-11 23:00:48 +0900

    module resolver 설치 및 적용
    관련 패키지를 설치하여 절대 경로 설정
    
    [패키지]
    -devDep-
    "babel-plugin-module-resolver": "^5.0.0"
    
    [내용]
    'baseUrl': '.'
    '@commons': './src/commons'
    '@screens': './src/screens'
    '@stacks': './src/stacks'
    '@redux': './src/redux'
    '@': './'

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,33 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['babel-plugin-styled-components'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          // prettier-ignore
+          alias: {
+            '@commons': './src/commons',
+            '@screens': './src/screens',
+            '@stacks': './src/stacks',
+            '@redux': './src/redux',
+            '@': './',
+          },
+        },
+      ],
+      'babel-plugin-styled-components',
+    ],
   };
 };

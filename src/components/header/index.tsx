bd5da4670dbd7035b9ec8610b72d6a623c5edb12
yeontan0826 <yeontan0826@gmail.com>
2023-09-21 16:14:28 +0900

    react-native-svg 라이브러리 설치 및 typescript 의존성 설정

---
+++ src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import { StackHeaderProps } from '@react-navigation/stack';
+import { Surface } from '@react-native-material/core';
+import { Ionicons } from '@expo/vector-icons';
+
+import * as S from './styles';
+import { colors } from '@styles/theme';
+import ClassumSvg from '@assets/classum.svg';
+
+const Header = ({ navigation }: StackHeaderProps): JSX.Element => {
+  const handleMoveToPreview = () => {
+    navigation.push('preview');
+  };
+
+  return (
+    <Surface elevation={4}>
+      <S.Container>
+        <ClassumSvg width={110} />
+        <S.PreviewButton activeOpacity={0.6} onPress={handleMoveToPreview}>
+          <Ionicons name="eye-outline" size={24} color={colors.gray} />
+          <S.PreviewLabel>미리보기</S.PreviewLabel>
+        </S.PreviewButton>
+      </S.Container>
+    </Surface>
+  );
+};
+
+export default Header;
